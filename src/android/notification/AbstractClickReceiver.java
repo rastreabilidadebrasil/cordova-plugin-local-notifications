e3874cd8bb6f730e5f23cbd57261aed4fdd7f8c0
gaetani <gaetani@gmail.com>
2017-01-16 11:47:08 -0200

    acerto das notificacoes

---
+++ src/android/notification/AbstractClickReceiver.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2013-2015 by appPlant UG. All rights reserved.
+ *
+ * @APPPLANT_LICENSE_HEADER_START@
+ *
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Apache License
+ * Version 2.0 (the 'License'). You may not use this file except in
+ * compliance with the License. Please obtain a copy of the License at
+ * http://opensource.org/licenses/Apache-2.0/ and read it before using this
+ * file.
+ *
+ * The Original Code and all software distributed under the License are
+ * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+ * Please see the License for the specific language governing rights and
+ * limitations under the License.
+ *
+ * @APPPLANT_LICENSE_HEADER_END@
+ */
+
+package de.appplant.cordova.plugin.notification;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Abstract content receiver activity for local notifications. Creates the
+ * local notification and calls the event functions for further proceeding.
+ */
+abstract public class AbstractClickReceiver extends BroadcastReceiver {
+
+    // Holds identifier of action most recently chosen  
+    // Null if notification was simply clicked
+    public String actionIdentifier = null;
+
+    /**
+     * Called when local notification was clicked to launch the main intent.
+     *
+     * @param context
+     *      Application context
+     * @param intent
+     *      Received intent with content data
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Bundle bundle   = intent.getExtras();
+
+        try {
+            String[] data = bundle.getStringArray(Options.EXTRA);
+            actionIdentifier = data[1];
+            JSONObject options = new JSONObject(data[0]);
+
+            Builder builder =
+                    new Builder(context, options);
+
+            Notification notification =
+                    buildNotification(builder);
+
+            onReceive(notification);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Called when local notification was clicked by the user.
+     *
+     * @param notification
+     *      Wrapper around the local notification
+     */
+    abstract public void onReceive (Notification notification);
+
+    /**
+     * Build notification specified by options.
+     *
+     * @param builder
+     *      Notification builder
+     */
+    abstract public Notification buildNotification (Builder builder);
+
+
+}
